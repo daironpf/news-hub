b32d1349e530d742407f89f623150760ae783593
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2023-02-01 20:00:07 -0600

    Add Eureka Client dependency to my Language Service
    Add and Config the Language Service to make him a client of eureka client to connect the discovery server

--- language-service/src/main/java/com/news/hub/languageservice/LanguageServiceApplication.java
+++ language-service/src/main/java/com/news/hub/languageservice/LanguageServiceApplication.java
@@ -3,6 +3,8 @@ package com.news.hub.languageservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class LanguageServiceApplication {
 
