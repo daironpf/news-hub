d4b5ed102213399fa31c852863ba73b3d5c05933
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2023-02-01 21:39:38 -0600

    update of module Tag Service

---
+++ tag-service/src/main/java/com/news/hub/tagservice/TagServiceApplication.java
@@ -0,0 +1,11 @@
+package com.news.hub.tagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TagServiceApplication.class, args);
+    }
+}
