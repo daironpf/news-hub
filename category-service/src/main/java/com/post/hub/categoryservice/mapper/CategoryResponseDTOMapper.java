244430f82d73e42a3aaf97aef35a0252211905e7
Dairon Pérez Frías <dairon.perezfrias@gmail.com>
2023-02-04 14:25:51 -0600

    feat: Add DTO Mapper for CategoryResponse + CategoryResponseDTOMapper mapper from Category model to CategoryResponse

---
+++ category-service/src/main/java/com/post/hub/categoryservice/mapper/CategoryResponseDTOMapper.java
@@ -0,0 +1,21 @@
+package com.post.hub.categoryservice.mapper;
+
+import com.post.hub.categoryservice.dto.CategoryResponse;
+import com.post.hub.categoryservice.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Service
+public class CategoryResponseDTOMapper implements Function<Category, CategoryResponse> {
+    @Override
+    public CategoryResponse apply(Category category) {
+        return new CategoryResponse(
+                category.getId(),
+                category.getName(),
+                category.getUrl(),
+                category.getDescription(),
+                category.getUrlIcon()
+        );
+    }
+}
